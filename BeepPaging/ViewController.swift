4b361befab9c438e792dadfc1d086e4811ffd2da
齐旭晨 <1023578360@qq.com>
2019-05-01 14:57:17 +0800

    Initial Commit

---
+++ BeepPaging/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BeepPaging
+//
+//  Created by 齐旭晨 on 2019/5/1.
+//  Copyright © 2019 齐旭晨. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
